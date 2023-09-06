56e4868ae54738c9918a2313198b99f76bb80240
lotusloli <nymphaeal@qq.com>
2023-09-06 18:08:16 +0800

    1. vite base url;

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 import VueRouter from 'unplugin-vue-router/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/front-list/',
   plugins: [
     VueRouter({
       routesFolder: './src/views',

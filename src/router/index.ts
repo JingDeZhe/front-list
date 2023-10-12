010d8f555528c0787a2343a10d31d038467e6a78
lotusloli <nymphaeal@qq.com>
2023-10-12 15:21:46 +0800

    1. remove layout;

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,7 @@
-import { setupLayouts } from 'virtual:generated-layouts'
 import { createRouter, createWebHashHistory } from 'vue-router/auto'
-import { routes } from 'vue-router/auto/routes'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
-  extendRoutes: (routes) => setupLayouts(routes),
 })
 
 export default router

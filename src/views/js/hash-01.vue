372234b9e47dfee5790f695920671ed9c2d6fcfa
lotusloli <nymphaeal@qq.com>
2023-10-24 17:45:14 +0800

    1. tmp;

---
+++ src/views/js/hash-01.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+class Pair {
+  public key: number
+  public value: string
+
+  constructor(key: number, value: string) {
+    this.key = key
+    this.value = value
+  }
+}
+
+class ArrayHashMap {
+  private readonly buckets: (Pair | null)[]
+
+  constructor() {
+    this.buckets = new Array(100).fill(null)
+  }
+
+  private hashFunc(key: number) {
+    return key % 100
+  }
+
+  public get(key: number): string | null {}
+}
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style lang="scss"></style>

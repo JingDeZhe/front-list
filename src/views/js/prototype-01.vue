1e682567b6ab5db6a87b106109b4000064140907
lotus <1504558766@qq.com>
2024-03-15 16:58:13 +0800

    1. add pts;

---
+++ src/views/js/prototype-01.vue
@@ -0,0 +1,18 @@
+<script lang="js" setup>
+function Role(name) {
+  this.name = name
+
+  this.say = function() {
+    console.log(`hello, I'm ${this.name}`)
+  }
+  return this
+}
+
+const snoopy = new Role('snoopy')
+snoopy.say()
+console.dir(snoopy)
+</script>
+
+<template></template>
+
+<style lang="scss"></style>
